cecfb6bff7845f9ae00b5b69db74efd9bda266c3
Adam Kulpa <ad200@gazeta.pl>
2018-06-21 23:15:48 +0100

    Change js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ Temat zadania:
  Podpis powinien pokazywać, ile razy kliknięto ten obraz. 
  Użyj jQuery aby to zrealizować - dodaj dla tego obrazu nasłuchiwacz zdarzeń click, który będzie powiększać zmienną numClicks oraz aktualizować podpis. 
  
-
+*/
     var numClicks = 0;
     
     $("#crocs").on("click", function(index) 
